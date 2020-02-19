51ce9ec94021afb164c02544c41c8415c46d36c7
lucie barbier <barbier.lucy@gmail.com>
2020-02-19 16:12:40 +0100

    exo completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+puts 'Creating restaurants...'
+
+restaurants_attributes = [
+  {
+    name:         'Pizzeria',
+    address:      'Lyon',
+    phone_number:  '50203040502',
+    category:      'italian'
+  },
+  {
+    name:         'The Chinese restaurant',
+    address:      'Paris',
+    phone_number:  '64654646046',
+    category:      'chinese'
+  },
+  {
+    name:         'Les frites',
+    address:      'Lille',
+    phone_number:  '12334324324',
+    category:      'belgian'
+  },
+  {
+    name:         'La Taverne',
+    address:      'Grenoble',
+    phone_number:  '69212399423',
+    category:      'french'
+  },
+  {
+    name:         'Okaido',
+    address:      'Grenoble',
+    phone_number:  '70496564946',
+    category:      'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
